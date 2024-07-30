20f113f3eb26443209bba84848db38016666db1e
DcoderLLC <dcoderllc@gmail.com>
2024-07-30 22:42:58 +0530

    Team page Init

---
+++ client/src/Components/Team/TeamHeader.jsx
@@ -0,0 +1,22 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const TeamHeader = () => {
+  return (
+    <Box>
+      <div class="container px-6 py-16 mx-auto text-center">
+        <div class="max-w-lg mx-auto">
+          <h1 class="text-3xl font-semibold text-gray-800 lg:text-4xl">
+            Building Your Next App with our Awesome components
+          </h1>
+          <p class="mt-6 text-gray-500 ">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero
+            similique obcaecati illum mollitia.
+          </p>
+        </div>
+      </div>
+    </Box>
+  );
+};
+
+export default TeamHeader;
--- client/src/Pages/Team.jsx
+++ client/src/Pages/Team.jsx
@@ -1,8 +1,12 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import TeamHeader from '../Components/Team/TeamHeader'
 
 const Team = () => {
   return (
-    <div>Team</div>
+    <Box>
+      <TeamHeader />
+    </Box>
   )
 }
 
