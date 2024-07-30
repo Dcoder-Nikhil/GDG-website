abb6b65a39e8b9779b4e7eb21843a4c73da57880
DcoderLLC <dcoderllc@gmail.com>
2024-07-30 10:07:02 +0530

    File Folder created and Routed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,33 @@
 import {} from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Box} from '@mui/material'
+import Home from "./Pages/Home"
+import Event from "./Pages/Events"
+import Gallery from "./Pages/Gallery"
+import Team from "./Pages/Team"
+import FAQ from "./Pages/FAQ"
+import JoinUs from "./Pages/JoinUs"
+import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
+
 
 function App() {
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <Box>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home/>} />
+          <Route path="/event" exact element={<Event/>} />
+          <Route path="/gallery" exact element={<Gallery/>} />
+          <Route path="/teams" exact element={<Team/>} />
+          <Route path="/faq" exact element={<FAQ/>} />
+          <Route path="/join" exact element={<JoinUs/>} />
+        </Routes>
+        <Footer />
+      </Router>
+    </Box>
   )
 }
 
