1f5a796b4e73fd8d4b98bd892601deadf510f6f9
DcoderLLC <dcoderllc@gmail.com>
2024-08-01 10:16:54 +0530

    JoinUs pafe complete

--- client/src/Pages/JoinUs.jsx
+++ client/src/Pages/JoinUs.jsx
@@ -1,8 +1,12 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import JoinHero from '../Components/JoinUs/JoinHero'
 
 const JoinUs = () => {
   return (
-    <div>JoinUs</div>
+    <Box>
+      <JoinHero />
+    </Box>
   )
 }
 
