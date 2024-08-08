e48e0fe73bf9557592536c7c2ea2f54479c83d91
DcoderLLC <dcoderllc@gmail.com>
2024-08-08 15:02:31 +0530

    Footer and Join Page updated

--- client/src/Components/JoinUs/JoinHero.jsx
+++ client/src/Components/JoinUs/JoinHero.jsx
@@ -5,25 +5,30 @@ import logo from "../../assets/logo.png";
 const JoinHero = () => {
   return (
     <Box className="bg-gray-900">
-      <div className="container flex flex-col px-6 py-8 mx-auto space-y-6 lg:h-[32rem] lg:py-10 lg:flex-row lg:items-center">
+      <div className="container flex flex-col px-14 py-8 mx-auto space-y-6 lg:h-[32rem] lg:py-10 lg:flex-row lg:items-center">
         <div className="w-full lg:w-1/2">
           <div className="lg:max-w-lg">
             <h1 className="text-3xl font-semibold tracking-wide text-gray-800 dark:text-white lg:text-4xl">
               Google Developers Group Patna
             </h1>
             <div>
-              <hr className="border-gray-200 md:mt-8 border-gray-700" />
-              <p className="text-white">
+              <hr className="border-white md:mt-8 border-gray-700"/>
+              <p className="text-white py-4">
                 We're on a mission to build the largest tech community in Bihar.
                 Join us on this journey to make it possible.
               </p>
-              <hr className="my-6 border-gray-200 md:mt-1 border-gray-700" />
+              <hr className="my-6 border-white md:mt-1 border-gray-700"/>
 
-              <p className="text-white">
+              <p className="text-white p-2">
                 Click on the Buttons Below to join the respective forums or
-                community.
+                community
               </p>
-              <div className="flex md:mx-10 my-6 gap-4">
+              <div className="flex justify-center py-2">
+                <button className="grid items-center border border-decoration-white rounded-lg p-2 text-white">
+                  GDG Patna Chapter
+                </button>
+              </div>
+              <div className="grid grid-cols-4 md:mx-10 my-6 gap-4">
                 <button className="border border-decoration-white rounded-lg p-2 text-white">
                   LinkedIn
                 </button>
@@ -53,7 +58,7 @@ const JoinHero = () => {
 
         <div className="flex items-center justify-center w-full h-96 lg:w-1/2">
           <img
-            className="object-cover w-full h-full mx-auto rounded-md lg:max-w-2xl"
+            className="object-cover w-full h-full mx-auto rounded-lg lg:max-w-2xl"
             src={logo}
             alt="gdg logo"
           />
